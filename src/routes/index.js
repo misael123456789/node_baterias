a599c6d7428af2486d0472b1552bf067f0617ea5
Misael <li.misael.sanchez@gmail.com>
2024-01-03 00:15:56 -0600

    TEST

--- src/routes/index.js
+++ src/routes/index.js
@@ -43,7 +43,7 @@ router.get('/models', (req, res)=>{
 });
 
 router.get('/find', (req, res)=>{
-    axios.get(LTH_URL+'/api/fitment/find', { params: { type: req.query.type, year: req.query.year, make: req.query.make, model: req.query.model, engine: req.query.engine} })
+    axios.get(LTH_URL+'/api/products/find', { params: { type: req.query.type, year: req.query.year, make: req.query.make, model: req.query.model, engine: req.query.engine} })
   .then(response => {
     const data = response.data;
     res.json(data)
