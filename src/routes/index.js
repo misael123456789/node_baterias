4b9ab36499e71f26fd26bf6216e671070b36c372
Misael <li.misael.sanchez@gmail.com>
2024-01-03 00:07:13 -0600

    TEST

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,19 @@ const router = Router()
 LTH_URL = 'https://www.lth.com.mx';
 
 router.get('/years', (req, res)=>{
-    axios.get(LTH_URL+'/api/fitment/years?type=Automotriz', { params: { type: req.query.type } })
+    axios.get(LTH_URL+'/api/fitment/years', { params: { type: req.query.type } })
+  .then(response => {
+    const data = response.data;
+    res.json(data)
+  })
+  .catch(error => {
+    console.log(error);
+  });
+    
+})
+
+router.get('/makes', (req, res)=>{
+    axios.get(LTH_URL+'/api/fitment/makes', { params: { type: req.query.type, year: req.query.year} })
   .then(response => {
     const data = response.data;
     res.json(data)
