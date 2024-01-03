cea997d591e1860440e870a8a91c4cbc95bb5424
Misael <li.misael.sanchez@gmail.com>
2024-01-02 23:19:23 -0600

    TEST

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,19 @@
 const {Router} = require('express')
+const request = require('request');
+const axios = require('axios');
 const router = Router()
 
 router.get('/', (req, res)=>{
-    res.json({"title":"Node server for google sheets",
-    "Version":"1.0.0",
-    "Node Version":process.version
-    })
+    axios.get('https://www.lth.com.mx/api/fitment/years?type=Automotriz')
+  .then(response => {
+    const data = response.data;
+    console.log(data);
+    res.send(data)
+  })
+  .catch(error => {
+    console.log(error);
+  });
+    
 })
 
 module.exports = router
